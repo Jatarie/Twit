51b6e9de244c8c41cf68482de12bd4b043e4b955
matt <matthew.e.j.smyth@gmail.com>
2020-10-28 21:35:35 +0000

    Updates

---
+++ precache-manifest.96c5685ab231187485d0f41d72d5e591.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dcbe93d8c5e587b74d55569a4fdebbd",
+    "url": "/TwitterClone/index.html"
+  },
+  {
+    "revision": "dcdfc2039b2aba61a0a7",
+    "url": "/TwitterClone/static/css/main.21b8abf3.chunk.css"
+  },
+  {
+    "revision": "d94da5cc515606a8e67b",
+    "url": "/TwitterClone/static/js/2.c423373d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TwitterClone/static/js/2.c423373d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcdfc2039b2aba61a0a7",
+    "url": "/TwitterClone/static/js/main.5ff57e4d.chunk.js"
+  },
+  {
+    "revision": "46d9935db71ca546b16d",
+    "url": "/TwitterClone/static/js/runtime-main.07fd6984.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TwitterClone/static/media/logo.5d5d9eef.svg"
+  }
+]);
